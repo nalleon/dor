5d1d66357a4316ea3da53b5096e5b0d0bc9e7b37
nalleon <nabil14716@gmail.com>
2025-10-27 19:36:29 +0000

    feat(ut3/a3): Working on a3

---
+++ ut2/a3/src/js/miniatura.js
@@ -0,0 +1,9 @@
+function init() {
+  let imgDefer = document.getElementsByTagName("img");
+  for (let i = 0; i < imgDefer.length; i++) {
+    if (imgDefer[i].getAttribute("data-src")) {
+      imgDefer[i].setAttribute("src", imgDefer[i].getAttribute("data-src"));
+    }
+  }
+}
+window.onload = init;
